f6d0bfc191ddf8daace19fe6d264df8d10b9ab9f
FranciscoP <franciscopresencia@hotmail.com>
2015-10-31 01:03:39 +0100

    Implemented the flags

--- test/index.js
+++ test/index.js
@@ -41,4 +41,54 @@ describe("Autoload", function() {
       }
     });
   });
+
+  it("accepts the option 'deep' as false", function(){
+    var tree = autoload("test/test-tree", { deep: false });
+
+    tree.should.eql({
+      '1': 1,
+      '2': 2,
+      'withDash': "with-dash"
+    });
+  });
+  
+  it("accepts the option 'deep' as true (same as default)", function(){
+    autoload("test/test-tree", { deep: true }).should.eql(autoload("test/test-tree"));
+  });
+
+  it("accepts the option 'js' as false", function(){
+    var tree = autoload("test/test-tree", { js: false });
+
+    tree.should.eql({
+      '3': {
+        '32': {
+          '321': {
+            value: 3
+          }
+        }
+      }
+    });
+  });
+  
+  it("accepts the option 'js' as true (same as default)", function(){
+    autoload("test/test-tree", { js: true }).should.eql(autoload("test/test-tree"));
+  });
+
+  it("accepts the option 'json' as false", function(){
+    var tree = autoload("test/test-tree", { json: false });
+
+    tree.should.eql({
+      '1': 1,
+      '2': 2,
+      'withDash': "with-dash",
+      '3': {
+        '31': 31,
+        '32': {}
+      }
+    });
+  });
+  
+  it("accepts the option 'json' as true (same as default)", function(){
+    autoload("test/test-tree", { json: true }).should.eql(autoload("test/test-tree"));
+  });
 });
